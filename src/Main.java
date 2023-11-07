f86aa464b235b3817dfc2749b0599d944ed2ae88
admin-rez <kraken_1111@mail.ru>
2023-11-07 08:56:26 +0300

    пишет все ошибку,я не понимаю

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Потребуется дней:" + calculateDeliveryDays(95));
         // Задание 1
         int currentYear = 2021;
-        boolean isLeapYear = false;
-
-        isLeapYear = isLeap(currentYear);
+        boolean isLeapYear = isLeap(currentYear);
 
         if (isLeapYear) {
             System.out.println("Год " + currentYear + " високосный");
@@ -13,54 +11,42 @@ public class Main {
             System.out.println("Год " + currentYear + " не високосный");
         }
 
-        versionSelector();
-        DeliveryDays();
+        recommendApplicationVersion();
+        calculateDeliveryDays();
     }
 
     static boolean isLeap(int year) {
-        if ((year % 4 == 0) && (year % 100 != 0)) {
-            return true;
-        } else if (year % 400 == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
 
     // Задание 2
-    public static void versionSelector() {
-        System.out.print("Текущий год (например, 2021):");
-        int currentYear = 2015;
-        System.out.print("Год выпуска вашего устройства (четный - iOS, нечетный - Android):");
-        int deviceYear = 2023;
-        if (deviceYear % 2 != 0) {
-            System.out.println("Похоже, у вас Android-устройство " + deviceYear +
-                    ". Если ваше устройство старше текущего года, мы рекомендуем установить облегченную версию.");
+
+    public static void recommendApplicationVersion() {
+        int clientDeviceYear = 2015;
+        if ((clientDeviceYear - deviceYear) > 0 && clientOS == 0) {
+            System.out.println("Вы можете скачать облегченную версию приложения");
+        } else if ((clientDeviceYear - deviceYear) > 0 && clientOS == 1) {
+            System.out.println("Вы можете скачать облегченную версию приложения");
         } else {
-            System.out.println("Похожо, у вас iOS-устройство " + deviceYear +
-                    ". Если ваше устройство старше текущего года, мы реккомендуем установить облегченную версию.");
+            System.out.println("Вы можете скачать полную версию приложения");
         }
     }
 
     // Задание 3
-    public static void DeliveryDays() {
 
-        int deliveryDistance = 95;
-        int days = 0;
 
-        if (deliveryDistance > 100) {
-            days = -1;
-        } else if (deliveryDistance <= 20) {
-            days = 1;
-        } else if (deliveryDistance < 60) {
-            days = 2;
+    public static int calculateDeliveryDays(int distance) {
+        if (distance > 100) {
+            return -1;
         } else {
-            days = 3;
+            int days = 1;
+            if (distance > 20) {
+                days++;
+            }
+            if (distance > 60) {
+                days++;
+            }
+            return days;
         }
-
-        System.out.println("Потребуется дней: " + days);
-
     }
 }
-
-
